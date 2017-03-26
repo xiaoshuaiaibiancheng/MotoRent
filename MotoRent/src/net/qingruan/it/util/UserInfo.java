6791b3c0a065e0f7019d30b39d59245981668eba
xiaoshuai <1669932644@qq.com>
2017-03-26 10:37:19 +0800

    first commit

---
+++ MotoRent/src/net/qingruan/it/util/UserInfo.java
@@ -0,0 +1,10 @@
+package net.qingruan.it.util;
+
+public class UserInfo {
+	private String username;
+	private String sex;
+	private Integer age;
+	public void printInfo(){
+		System.out.println("0000000");
+	}
+}
